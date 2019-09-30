0e3770481e09cece7a19340857cc92ccc8daa39e
Afif Firmansyah <afif_173040115@mail.unpas.ac.id>
2019-09-30 13:15:22 +0700

    Merubah isi file constuktor.php dengan menambahkan konstruktor dan pemanggilan nya

--- construktor.php
+++ construktor.php
@@ -14,27 +14,22 @@
         public function getLabel(){
                 return "$this->penulis , $this->penerbit"; 
         }
+
+        // Membuat konstruktor
+        public function __contruct($judul, $penulis, $penerbit, $harga){
+            $this->$judul = $judul;
+            $this->$penulis = $penulis;
+            $this->$penerbit = $penerbit;
+            $this->$harga = $harga;
+        }
+        // end konstruktor
     }
-    
-    
-        
 
-        $produk3 = new Produk();
-        $produk3->judul    = "Naruto";
-        $produk3->penulis  = "Masashi Kishimoto";
-        $produk3->penerbit = "Shonen Jump";
-        $produk3->harga    = 30000;
-        var_dump($produk3);
-
-
-        $produk4 = new Produk();
-        $produk4->judul    = "Uncharted";
-        $produk4->penulis  = "Neil Druckman";
-        $produk4->penerbit = "sony Computer";
-        $produk4->harga    = 2500000;
-
-        echo "Komik : " . $produk3->getLabel();
+        $produk1 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump",30000);
+        $produk2 = new Produk("Uncharted","Neil Druckman","sony Computer",2500000);
+     
+        echo "Komik : " . $produk1->getLabel();
         echo "<br>";
-        echo "Game : " . $produk4->getLabel();
+        echo "Game : " . $produk2->getLabel();
 
 ?> 

aafedf489e211ee41019bfcc280a518349392ece
Afif Firmansyah <afif_173040115@mail.unpas.ac.id>
2019-10-01 17:41:05 +0700

    Merubah isi file yang terdapat di folder namespace

--- namespace/index.php
+++ namespace/index.php
@@ -2,13 +2,21 @@
 
     require_once 'App/init.php';
     
-    $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
-    $produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+    // $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+    // $produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
     
-    $cetakProduk = new CetakInfoProduk();
-    $cetakProduk->tambahProduk( $produk1 );
-    $cetakProduk->tambahProduk( $produk2 );
+    // $cetakProduk = new CetakInfoProduk();
+    // $cetakProduk->tambahProduk( $produk1 );
+    // $cetakProduk->tambahProduk( $produk2 );
    
-    echo $cetakProduk->cetak();
-    echo "<hr>";
-    new Coba();
+    // echo $cetakProduk->cetak();
+    // echo "<hr>";
+   
+   
+    USE App\Service\User as ServiceUser;
+    USE App\Produk\User as ProdukUser;
+
+    new ServiceUser();
+    echo "<br>";
+    new ProdukUser();
+ 
